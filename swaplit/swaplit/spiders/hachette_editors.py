7b36d6b49ac83b1916853fc1643af38333547691
romain isoz <isozromain@gmail.com>
2024-10-05 19:19:27 +0100

    spider crawl between all pages within the Editors page

--- swaplit/swaplit/spiders/hachette_editors.py
+++ swaplit/swaplit/spiders/hachette_editors.py
@@ -6,12 +6,24 @@ class HachetteEditorsSpider(scrapy.Spider):
     download_delay = 10
     allowed_domains = ["hachette.fr"]
     start_urls = ["https://www.hachette.fr/editeurs"]
+    page_nb = 1
 
     def parse(self, response):
         editor_links = response.css('div.field-name-hw-editeur-logo a::attr(href)').getall()
 
         for editor in editor_links:
             yield response.follow(editor, callback=self.parse_editor)
+
+        if self.page_nb == 1:
+            next_link = response.css('ul.pagination a::attr(href)').get()
+        elif self.page_nb > 3:
+            return
+        else:
+            next_link = response.css('ul.pagination a::attr(href)').getall()[1 + self.page_nb]
+
+        self.page_nb += 1
+
+        yield response.follow(next_link, callback=self.parse)
 
 
     def parse_editor(self, response):
