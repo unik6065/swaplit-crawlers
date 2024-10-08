fb65b32b12eea5647c92cbb75adbb70f526b9681
romain isoz <isozromain@gmail.com>
2024-10-08 10:17:42 +0100

    Add pipelines to export data to CSV file and the upload it within a swift container

--- swaplit/swaplit/pipelines.py
+++ swaplit/swaplit/pipelines.py
@@ -9,13 +9,65 @@
 import swiftclient
 import os
 from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import CsvItemExporter
 
 
-class SwaplitPipeline:
+class CSVExportItemPipeline:
+    def open_spider(self, spider):
+        self.exporters = {}
+        self.csv_file_path = f'{spider.name}.csv'
+
+    def close_spider(self, spider):
+        for exporter, csv_file in self.exporters.values():
+            exporter.finish_exporting()
+            csv_file.close()
+
+    def _exporter_for_item(self, item):
+        csv_file = open(self.csv_file_path, "wb")
+        exporter = CsvItemExporter(csv_file)
+        exporter.start_exporting()
+        self.exporters[csv_file] = (exporter, csv_file)
+        return self.exporters[csv_file][0]
+
     def process_item(self, item, spider):
+        exporter = self._exporter_for_item(item)
+        exporter.export_item(item)
         return item
 
+class SwaplitPipeline:
+    def open_spider(self, spider):
+        # Se connecter à OpenStack Swift
+        self.container = spider.settings.get('DATA_CONTAINER')
+        self.conn = swiftclient.Connection(
+            auth_version='3',  # OpenStack Identity API v3
+            user=os.getenv('OS_USERNAME'),
+            key=os.getenv('OS_PASSWORD'),
+            authurl=os.getenv('OS_AUTH_URL'),
+            os_options={
+                'project_id': os.getenv('OS_PROJECT_ID'),
+                'user_domain_name': os.getenv('OS_USER_DOMAIN_NAME'),
+                'region_name': os.getenv('OS_REGION_NAME')
+            }
+        )
+        self.file_name = f'{spider.name}.csv'
+
+    def process_item(self, item, spider):
+        return item
+    def close_spider(self, spider):
+        with open(self.file_name, 'rb') as csv_file:
+            self.conn.put_object(
+                self.container,
+                self.file_name,
+                contents=csv_file,
+                content_type='text/csv'
+            )
+        os.remove(self.file_name)
+        # Retrieve the URL of the uploaded image
+        self.conn.close()
+
+
 class SwiftImagesPipeline(ImagesPipeline):
+
     def open_spider(self, spider):
         # Se connecter à OpenStack Swift
         self.spiderinfo = self.SpiderInfo(spider)
