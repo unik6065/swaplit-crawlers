cdb9ec17a26fadc22ccd054682c63a169c832cb3
romain isoz <isozromain@gmail.com>
2024-10-13 21:25:59 +0100

    changed the categories_files path within the helper

--- swaplit/swaplit/helpers/categories_helper.py
+++ swaplit/swaplit/helpers/categories_helper.py
@@ -1,8 +1,9 @@
 import yaml
+import os
 
 class CategoryHelper:
     def __init__(self, spider_name):
-        self.__categories_file = '../categories/' + spider_name + '-categories.yaml'
+        self.__categories_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'categories', spider_name + '-categories.yaml')
 
         with open(self.__categories_file, 'r') as file:
             self.__category_service = yaml.safe_load(file)
